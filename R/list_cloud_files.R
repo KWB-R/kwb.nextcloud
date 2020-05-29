f8466947d2e248dfb81394cd75d0fc74d11ae07d
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2020-05-29 13:54:06 +0200

    Back to method 1, convert types (#1, #3)
    method 2 using xpaths fails if properties are
    not available for all elements

--- R/list_cloud_files.R
+++ R/list_cloud_files.R
@@ -1,12 +1,12 @@
 if (FALSE)
 {
-  urls <- kwb.nextcloud:::get_nextcloud_urls("hsonne", path = "projects")
+  path <- "proposals/bmbf_digital/Previous-projects/Budget/10_Filled_out_forms"
+  path <- "departments"
 
-  info_1 <- kwb.nextcloud:::list_files("departments", method = 1L)
-  info_2 <- kwb.nextcloud:::list_files("departments", method = 2L)
+  infos <- lapply(1:2, function(m) kwb.nextcloud:::list_files(path, method = m))
 
-  View(info_1)
-  View(info_2)
+  View(infos[[1]])
+  View(infos[[2]])
 
   inspect <- function(xx) writeLines(as.character(xx))
 
@@ -44,19 +44,18 @@ download_files <- function(
 
 # list_files -------------------------------------------------------------------
 list_files <- function(
-  path, user = nextcloud_user(), password = nextcloud_password(), method = 2L
+  path, user = nextcloud_user(), password = nextcloud_password(), method = 1L
 )
 {
   stopifnot(method %in% 1:2)
 
+  #kwb.utils::assignPackageObjects("kwb.nextcloud")
   #path = "proposals/bmbf_digital/Previous-projects/Budget/10_Filled_out_forms"
-  #user = nextcloud_user()
-  #password = nextcloud_password()
+  #user = nextcloud_user();password = nextcloud_password();method=1L
 
   urls <- get_nextcloud_urls(user, path = path)
 
-  body <- if (method == 2L) {
-    '<d:propfind xmlns:d="DAV:" xmlns:oc="http://owncloud.org/ns" xmlns:nc="http://nextcloud.org/ns">
+  body <- '<d:propfind xmlns:d="DAV:" xmlns:oc="http://owncloud.org/ns" xmlns:nc="http://nextcloud.org/ns">
     <d:prop>
     <d:getlastmodified />
     <d:getetag />
@@ -73,7 +72,6 @@ list_files <- function(
     <oc:share-types />
     </d:prop>
     </d:propfind>'
-  } # else NULL implicitly
 
   content <- parsed_propfind(urls$url_files, user, password, body = body)
 
@@ -83,24 +81,42 @@ list_files <- function(
 
     result <- parse_xml_content_1(content)
 
-    result$bytes <- to_numeric(result$bytes)
-    result$size <- to_numeric(result$size)
-    result$path <- substr(
+    result$href <- substr(
       x = result$href,
-      start = nchar(urls$user_files) + 3L,
+      start = nchar(urls$user_files) + nchar(dirname(path)) + 4L,
       stop = nchar(result$href)
     )
-    result$etag <- gsub('"', '', result$etag)
+
+    result$lastmodified <- to_posix(x = result$lastmodified)
+    result$etag <- gsub('"', "", result$etag)
+    result$fileid <- to_numeric(result$fileid)
+    result$size <- to_numeric(result$size)
+    result$has_preview <- result$has_preview != "false"
+    result$favorite <- to_numeric(result$favorite)
+    result$comments_unread <- to_numeric(result$comments_unread)
+
     result
 
   } else if (method == 2L) {
 
     result <- parse_xml_content_2(content)
-    View(result)
 
     result
   }
 
+  structure(result, path = path)
+}
+
+# to_posix ---------------------------------------------------------------------
+to_posix <- function(x)
+{
+  stopifnot(is.character(x), all(grepl("GMT$", x)))
+
+  locale <- Sys.getlocale("LC_TIME")
+  on.exit(Sys.setlocale("LC_TIME", locale))
+  Sys.setlocale("LC_TIME", "C")
+
+  as.POSIXct(x, format = "%a, %d %b %Y %H:%M:%S GMT", tz = "GMT")
 }
 
 # parse_xml_content_1 ----------------------------------------------------------
@@ -108,25 +124,49 @@ parse_xml_content_1 <- function(content)
 {
   x_all <- xml2::as_list(content)
 
+  first_and_only <- function(x) {
+    stopifnot(is.list(x))
+    n <- length(x)
+    if (n == 0L) {
+      return("")
+    }
+    if (n == 1L) {
+      x <- x[[1L]]
+    }
+    if (length(x) == 0L) {
+      return("")
+    }
+    stopifnot(length(x) == 1L)
+    x
+  }
+
   get_file_info <- function(x) {
 
-    #x <- x_all$multistatus[[1L]]
+    #x <- responses[[1L]]
 
-    prop <- x$propstat$prop
-    prop_1 <- function(name) kwb.utils::defaultIfNULL(prop[[name]][[1L]], "")
+    p <- x$propstat$prop
 
     kwb.utils::noFactorDataFrame(
-      etag = prop$getetag[[1L]],
-      href = x$href[[1L]],
-      status = x$propstat$status[[1L]],
-      modified = prop_1("getlastmodified"),
-      bytes = prop_1("quota-used-bytes"),
-      size = prop_1("getcontentlength")
+      href = first_and_only(x$href),
+      status = first_and_only(x$propstat$status),
+      lastmodified = first_and_only(p$getlastmodified),
+      etag = first_and_only(p$getetag),
+      resourcetype = first_and_only(p$resourcetype),
+      fileid = first_and_only(p$fileid),
+      permissions = first_and_only(p$permissions),
+      size = first_and_only(p$size),
+      has_preview = first_and_only(p$`has-preview`),
+      favorite = first_and_only(p$favorite),
+      comments_unread = first_and_only(p$`comments-unread`),
+      owner_display_name = first_and_only(p$`owner-display-name`),
+      share_types = first_and_only(p$`share-types`)
     )
   }
 
   #x <- x_all$multistatus[[1]]
-  do.call(rbind, lapply(unname(x_all$multistatus), get_file_info))
+  responses <- unname(x_all$multistatus)
+
+  do.call(rbind, lapply(responses, get_file_info))
 }
 
 # parse_xml_content_2 ----------------------------------------------------------
