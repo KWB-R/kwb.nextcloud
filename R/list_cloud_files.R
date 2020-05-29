c14ef1e3b403a26aed8f177c1cfe8db0edbf839f
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2020-05-29 17:42:00 +0200

    Move environment access functions to own file

--- R/list_cloud_files.R
+++ R/list_cloud_files.R
@@ -218,21 +218,3 @@ nextcloud_auth <- function(
 {
   httr::authenticate(user, password)
 }
-
-# nextcloud_user ---------------------------------------------------------------
-nextcloud_user <- function()
-{
-  Sys.getenv("NEXTCLOUD_USER")
-}
-
-# nextcloud_password -----------------------------------------------------------
-nextcloud_password <- function()
-{
-  Sys.getenv("NEXTCLOUD_PASSWORD")
-}
-
-# nextcloud_url ----------------------------------------------------------------
-nextcloud_url <- function()
-{
-  Sys.getenv("NEXTCLOUD_URL")
-}
