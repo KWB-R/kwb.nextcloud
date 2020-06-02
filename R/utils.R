a2e49687b6159b854c75c77e5e4fb1ace36374ce
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2020-06-03 00:16:23 +0200

    Move all helper functions to utils.R

--- R/utils.R
+++ R/utils.R
@@ -12,10 +12,28 @@ create_download_dir <- function(pattern)
   ))
 }
 
+# dav_path ---------------------------------------------------------------------
+dav_path <- function()
+{
+  "/remote.php/dav"
+}
+
 # decode_url -------------------------------------------------------------------
 decode_url <- function(x, to = "latin1")
 {
   iconv(utils::URLdecode(x), from = "UTF-8", to = to)
+}
+
+# fileid_to_version_href -------------------------------------------------------
+fileid_to_version_href <- function(fileid = "", user = nextcloud_user())
+{
+  file.path(dav_path(), "versions", user, "versions", fileid)
+}
+
+# href_to_url ------------------------------------------------------------------
+href_to_url <- function(href)
+{
+  file.path(nextcloud_url(), href)
 }
 
 # indent -----------------------------------------------------------------------
@@ -48,6 +66,12 @@ key_value <- function(key, value, .list = NULL)
   }
 
   paste0(key, "=\"", value, "\"")
+}
+
+# path_to_file_href ------------------------------------------------------------
+path_to_file_href <- function(path = "", user = nextcloud_user())
+{
+  file.path(dav_path(), "files", user, path)
 }
 
 # remove_leading_slashes -------------------------------------------------------
