4622b35d1844179f75f5adb8f7dd624493e57794
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2020-06-04 01:07:24 +0200

    Improve test script
    * create a folder
    * upload a file into the folder (several times)
    * check the versions
    * download the versions
    * delete the file
    * delete the folder

--- R/.test_put-delete.R
+++ R/.test_put-delete.R
@@ -1,59 +1,107 @@
+# Make also private functions available
 kwb.utils::assignPackageObjects("kwb.nextcloud")
 
-file_info <- exclude_directories(list_files("documents", full_info = TRUE))
+# Are we the test user?
+nextcloud_user()
 
+# Ok, we seem to be the test user, let's mess around!
+
+# List the files and folders on the main level
+list_files()
+
+# Get the file and folder properties
+file_info <- list_files(full_info = TRUE)
+file_info
+
+# Show only the files
+file_info <- exclude_directories(file_info)
+file_info
+
+# Relative paths to the files
 file_info$file
 
+# Download the files
+download_files(file_info$href)
+
+# Create a folder on the cloud
+create_folder("testfolder-1")
+
+# Did the folder arrive?
+"testfolder-1/" %in% list_files()
+
+# Path to a local temporary file
 file <- file.path(tempdir(), "testfile.txt")
 
-writeLines("Hi folks version 1!", file)
-readLines(file) # target_path = "documents" by default
-upload_file(file)
+# Path to target folder on the cloud
+target_path <- "testfolder-1"
 
+# Upload first version of the file
+writeLines("Hi folks version 1!", file)
+readLines(file)
+upload_file(file, target_path)
+
+# Upload second version of the file
 writeLines("Hi folks version 2!", file)
 readLines(file)
-upload_file(file, "documents")
+upload_file(file, target_path)
 
+# Upload third version of the file
 writeLines("Hi folks version 3!", file)
 readLines(file)
-upload_file(file, "documents")
+upload_file(file, target_path)
 
-arguments <- list(
-  file = "~/../Downloads/model-video/modelvideo_excelcursor.MOV",
-  target_path = "documents"
-)
+# Get the fileid of the uploaded file
+file_info <- list_files(target_path, full_info = TRUE)
 
-do.call(upload_file, arguments)
+fileid <- file_info$fileid[file_info$file == "testfile.txt"]
+fileid
 
-# Show the versions
-file_info <- exclude_directories(list_files(
-  arguments$target_path, "MOV$", full_info = TRUE
-))
+# Show the versions of the file (identified by fileid)
+# Ignore the warnings
+version_info <- get_version_info(fileid)
 
-stopifnot(identical(file_info$file, "modelvideo_excelcursor.MOV"))
+# There are two versions (the current version is not shown!)
+version_info
 
-version_info <- get_one_version_info(file_info$fileid)
+# Download all versions (unfortunately named by timestamp)
+downloaded_files <- download_files(version_info$href)
+downloaded_files
 
-# Download all versions
-download_files(version_info$href)
+# Read the contents (should be version 1 and 2)
+lapply(downloaded_files, readLines)
 
 # Delete a specific version? This seems to be not working
-nextcloud_request(version_info$href[1], "DELETE") # -> error
+nextcloud_request(version_info$href[1], "DELETE", really = real_trick)
 
 # Delete the file
 # I am using the string constant instead of "href" to see what I do!
 nextcloud_request(
-  "/remote.php/dav/files/hsonne/documents/test-2.txt",
-  "DELETE"
-)
-
-nextcloud_request(
-  "/remote.php/dav/files/hsonne/documents/modelvideo_excelcursor.MOV",
-  "DELETE"
+  href = path_to_file_href("testfolder-1/testfile.txt"),
+  "DELETE",
+  really = real_trick
 )
 
 # File deleted? Yes!
-list_files("documents")
+list_files("testfolder-1")
 
 # All versions deleted? Yes!!!
-get_version_info(file_info$fileid) # -> error
+get_version_info(fileid) # -> error
+
+# Upload the testfile again
+upload_file(file, target_path)
+
+# Check for success
+list_files(target_path)
+
+# Now try to delete the whole directory
+nextcloud_request(
+  href = path_to_file_href("testfolder-1"),
+  "DELETE",
+  really = real_trick
+)
+
+# Try to list again
+list_files(target_path) # -> not found (any more)!
+
+# List the upper level -> no more testfolder!
+list_files()
