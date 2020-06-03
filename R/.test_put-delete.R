9f1a240ea00e66dbbf4a123b79dd9b613d5c1fc1
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2020-06-03 22:46:34 +0200

    Update test script

--- R/.test_put-delete.R
+++ R/.test_put-delete.R
@@ -1,25 +1,38 @@
 kwb.utils::assignPackageObjects("kwb.nextcloud")
 
-file_info <- exclude_directories(
-  list_files("documents", "txt$", full_info = TRUE)
+file_info <- exclude_directories(list_files("documents", full_info = TRUE))
+
+file_info$file
+
+file <- file.path(tempdir(), "testfile.txt")
+
+writeLines("Hi folks version 1!", file)
+readLines(file) # target_path = "documents" by default
+upload_file(file)
+
+writeLines("Hi folks version 2!", file)
+readLines(file)
+upload_file(file, "documents")
+
+writeLines("Hi folks version 3!", file)
+readLines(file)
+upload_file(file, "documents")
+
+arguments <- list(
+  file = "~/../Downloads/model-video/modelvideo_excelcursor.MOV",
+  target_path = "documents"
 )
 
-nrow(file_info)
+do.call(upload_file, arguments)
 
-path <- "documents/test-1.txt"
-href <- path_to_file_href(path)
+# Show the versions
+file_info <- exclude_directories(list_files(
+  arguments$target_path, "MOV$", full_info = TRUE
+))
 
-nextcloud_request(href, "PUT", body = "Hi folks version 1!")
-nextcloud_request(href, "PUT", body = "Hi folks version 2!")
-nextcloud_request(href, "PUT", body = "Hi folks version 3!")
+stopifnot(identical(file_info$file, "modelvideo_excelcursor.MOV"))
 
-file_info <- exclude_directories(
-  list_files(dirname(path), "txt$", full_info = TRUE)
-)
-
-stopifnot(identical(file_info$file, "test-1.txt"))
-
-version_info <- get_version_info(file_info$fileid)
+version_info <- get_one_version_info(file_info$fileid)
 
 # Download all versions
 download_files(version_info$href)
@@ -29,7 +42,15 @@ nextcloud_request(version_info$href[1], "DELETE") # -> error
 
 # Delete the file
 # I am using the string constant instead of "href" to see what I do!
-nextcloud_request("/remote.php/dav/files/hsonne/documents/test-1.txt", "DELETE")
+nextcloud_request(
+  "/remote.php/dav/files/hsonne/documents/test-2.txt",
+  "DELETE"
+)
+
+nextcloud_request(
+  "/remote.php/dav/files/hsonne/documents/modelvideo_excelcursor.MOV",
+  "DELETE"
+)
 
 # File deleted? Yes!
 list_files("documents")
