606a6a9fc9af99dc9337efc6c2fd44fd2edd8d67
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2022-06-03 17:41:03 +0200

    Clean move_file_or_folder()
    - reindent,
    - integrate expressions instead of intermediate
      variables
    - TODO: What should the function return?

--- R/move_file_or_folder.R
+++ R/move_file_or_folder.R
@@ -4,36 +4,42 @@
 #'
 #' @param path path to the file or folder to be moved
 #' @param path_target path to the target file or folder to be moved to
-#' @param overwrite overwrite if file/folder is already existing (default: FALSE)
+#' @param overwrite whether to overwrite the file or folder in case that the
+#'   file or folder already exists (default: FALSE)
 #' @param \dots further arguments passed to
 #'   \code{kwb.nextcloud:::nextcloud_request}
 #' @param dbg print debug messages (default: TRUE)
 #' @inheritParams list_files
 #' @export
 #' @importFrom kwb.utils catAndRun
-move_file_or_folder <- function(path,
-                                path_target,
-                                overwrite = FALSE,
-                                user = nextcloud_user(),
-                                auth = nextcloud_auth(),
-                                dbg = TRUE,
-                                ...)
+move_file_or_folder <- function(
+  path,
+  path_target,
+  overwrite = FALSE,
+  user = nextcloud_user(),
+  auth = nextcloud_auth(),
+  dbg = TRUE,
+  ...
+)
 {
-  href = path_to_file_href(path)
-
-  url_target <- href_to_url(path_to_file_href(path_target))
-  msg <- sprintf("Moving file/folder from '%s' to '%s'",
-                 path,
-                 path_target)
-  kwb.utils::catAndRun(msg, expr = {
-  response <- nextcloud_request(href,
-                                "MOVE",
-                                auth = auth,
-                                headers = list(Destination = url_target,
-                                               Overwrite = ifelse(overwrite,
-                                                                  "T",
-                                                                  "F")),
-                                ...)},
-  dbg = dbg
+  response <- kwb.utils::catAndRun(
+    messageText = sprintf(
+      "Moving file/folder from '%s' to '%s'", path, path_target
+    ),
+    dbg = dbg,
+    expr = {
+      nextcloud_request(
+        href = path_to_file_href(path),
+        verb = "MOVE",
+        auth = auth,
+        headers = list(
+          Destination = href_to_url(path_to_file_href(path_target)),
+          Overwrite = ifelse(overwrite, "T", "F")
+        ),
+        ...
+      )
+    }
   )
+
+  # What should the function return?
 }
